191a65ae7ceab4f3ca5d73492778467308bed812
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2021-07-30 03:30:34 +0100

    starting portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Container, Grid } from "@material-ui/core";
+import Profile from "./components/Profile/Profile";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Resume from "./pages/Resume/Resume";
+
+const useStyles = makeStyles((theme) => ({
+  redBackground: {
+    background: "red",
+  },
+  blueBackground: {
+    background: "blue",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={3}
+          style={{ backgroundColor: "blue" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Profile />
+        </Grid>
+        <Grid item xs style={{ backgroundColor: "red" }}>
+          <Header />
+          <Portfolio />
+          <Resume />
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Profile = () => {
+  return <div>This is a Profile component</div>;
+};
+
+export default Profile;
