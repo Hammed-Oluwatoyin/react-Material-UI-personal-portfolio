9aedfb943daaa524ffe4bae0c568b1a54e31f15c
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2021-08-22 10:05:16 +0100

    resumeData

--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -76,6 +76,49 @@ const resumeData = {
     },
   },
 
+  services: [
+    {
+      title: "Frontend Development",
+      description: "i have been badass web developer for 10 years",
+    },
+
+    {
+      title: "Backend Development",
+      description:
+        "Building of sophisticated RESTful API, and GraphQL API, and ",
+    },
+
+    {
+      title: "Fullstack Web Development",
+      description: "Building fullstack Applications from Start to finish",
+    },
+  ],
+
+  skills: [
+    {
+      title: "FRONT-END",
+      description: [
+        "ReactJS",
+        "JavaScript",
+        "TypeScript",
+        "BootStrap",
+        "Material UI",
+      ],
+    },
+    {
+      title: "BACK-END",
+      description: ["NodeJS", "Java", "Python", "Solidity"],
+    },
+    {
+      title: "DATABASES",
+      description: ["firebase", "Ms SQL Server", "NySQL", "MongoDB"],
+    },
+    {
+      title: "SOURCE CONTROL",
+      description: ["Git", "Github", "MongoDb"],
+    },
+  ],
+
   about: `When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me.
 
  When I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath of that universal love which bears and sustains us, as it floats around us
